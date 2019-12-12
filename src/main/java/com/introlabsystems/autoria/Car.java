8f31f6fb81ded416c728f543db45276a8e958441
Abionics <alex.ermolaev@introlab-systems.com>
2019-12-12 16:01:14 +0200

    Scrap page title

--- src/main/java/com/introlabsystems/autoria/Car.java
+++ src/main/java/com/introlabsystems/autoria/Car.java
@@ -9,9 +9,12 @@ import java.io.IOException;
 public class Car {
     static void scrap(String url) throws IOException {
         var document = Jsoup.connect(url).get();
+        String title = document.title();
+        int to = title.indexOf(" от");
+        String name = title.substring(17, to);
         String info = document.selectFirst("div.review-text > p").text().strip();
         var writer = new BufferedWriter(new FileWriter(AutoRio.RESULT_FILE, true));
-        writer.write("\"" + url + "\", \"" + info + "\"\n");
+        writer.write("\"" + url + "\",\"" + name + "\",\"" + info + "\"\n");
         writer.close();
     }
 }
